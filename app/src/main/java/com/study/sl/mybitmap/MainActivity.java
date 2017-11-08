8407367274f12ad6c0f08fa5808ef75b425b3e42
juncle113 <64015323@qq.com>
2017-11-09 07:06:40 +0800

    添加通过采样率压缩图片

--- app/src/main/java/com/study/sl/mybitmap/MainActivity.java
+++ app/src/main/java/com/study/sl/mybitmap/MainActivity.java
@@ -1,13 +1,75 @@
 package com.study.sl.mybitmap;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ImageView_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        ImageView_image = (ImageView) findViewById(R.id.ImageView_image);
+
+        showMaxMemory();
+        setImage();
+    }
+
+    // 查看应用程序最高可用内存
+    private void showMaxMemory() {
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024 / 1024);
+        Log.d("TAG", "Max memory is " + maxMemory + "MB");
+    }
+
+    private void setImage() {
+        Bitmap bitmap = decodeSampledBitmapFromResource(getResources(), R.drawable.image01, 100, 100);
+
+        ImageView_image.setImageBitmap(bitmap);
+    }
+
+    // 通过设置采样率压缩图片
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+        // 第一次解析将inJustDecodeBounds设置为true，来获取图片大小
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        // 调用上面定义的方法计算inSampleSize值
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        // 使用获取到的inSampleSize值再次解析图片
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    // 计算采样率
+    public static int calculateInSampleSize(BitmapFactory.Options options,
+                                            int reqWidth, int reqHeight) {
+        // 原图片的高度和宽度
+        final int width = options.outWidth;
+        final int height = options.outHeight;
+
+        int inSampleSize = 1;
+
+        if (width > reqWidth || height > reqHeight) {
+            final int halfWidth = width / 2;
+            final int halfHeight = height / 2;
+
+            while ((halfWidth / inSampleSize) >= reqWidth && (halfHeight / inSampleSize) >= reqHeight) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
     }
 }
